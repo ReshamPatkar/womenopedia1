a7a717f9dc742e01b63ffb9bc9c586d5e47bf20c
Saniya230301 <care230301@gmail.com>
2022-04-17 20:09:52 +0530

    Diet Changes

--- lib/Screens/DietRec.dart
+++ lib/Screens/DietRec.dart
@@ -6,27 +6,49 @@ import '../model/meal.dart';
 class MealDetailScreen extends StatelessWidget {
   static String id = 'Meal Detail Screen';
   final Meal meal=Meal(
-      mealTime: "BREAKFAST",
-      name: "Fruit Granola",
+      mealTime: "DIET RECOMMENDATIONS",
+      name: "",
       kiloCaloriesBurnt: "271",
       timeTaken: "10",
       imagePath: "images/fruit_granola.jpg",
       ingredients: [
-        "1 cup of granola",
-        "1 banana",
-        "1/2 cup of raisins",
-        "1 tbsp of honey",
+        "Diet for Anti-Cancer ",
+        "Diet for HIV_AIDS ",
+        "Diet for Diabetes ",
+        "Diet for Pregnant Women ",
       ],
       preparation:
-      '''Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum tempus egestas sed sed risus pretium quam. Platea dictumst quisque sagittis purus sit. Senectus et netus et malesuada fames ac turpis egestas sed. Nibh cras pulvinar mattis nunc. Amet consectetur adipiscing elit duis tristique. Interdum velit euismod in pellentesque massa placerat duis ultricies lacus. Lectus proin nibh nisl condimentum id venenatis. Sit amet massa vitae tortor condimentum lacinia quis vel eros. Sed adipiscing diam donec adipiscing tristique risus nec feugiat in. Enim tortor at auctor urna. In dictum non consectetur a.
+      '''An anti-cancer diet is an important strategy you can use to reduce your risk of cancer. 
+•	Eat plenty of fruits and vegetables. 
+•	Sip green tea throughout your day
+•	Eat more tomatoes. 
+•	Use olive oil. 
+•	Snack on grapes. 
+•	Use garlic and onions abundantly. 
+•	Eat fish. 
 
-Sit amet risus nullam eget. Viverra adipiscing at in tellus integer feugiat scelerisque varius morbi. Quis risus sed vulputate odio ut enim blandit volutpat maecenas. Turpis cursus in hac habitasse platea dictumst quisque sagittis. Eget mi proin sed libero. Non diam phasellus vestibulum lorem sed risus ultricies tristique. Neque viverra justo nec ultrices dui. Gravida neque convallis a cras semper. Morbi tempus iaculis urna id volutpat lacus laoreet non. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum. Amet mattis vulputate enim nulla aliquet. Lectus urna duis convallis convallis tellus. Porta non pulvinar neque laoreet suspendisse interdum consectetur libero id. Diam ut venenatis tellus in metus vulputate eu scelerisque felis. Adipiscing vitae proin sagittis nisl. Sed libero enim sed faucibus turpis in. Pulvinar mattis nunc sed blandit. Nisi scelerisque eu ultrices vitae auctor eu augue ut. Diam volutpat commodo sed egestas egestas fringilla phasellus faucibus. Sed id semper risus in hendrerit.
+\n Diet recommendations for HIV/AIDS Patients
+•	Vitamin A and beta-carotene (skin and lung health): 
+•	Meat, fish, chicken, nuts, beans, avocados, broccoli, and green leafy vegetables.
+•	Vitamin C, citrus fruits.
+•	Iron, grain bread, liver, fish, eggs.
+•	Selenium and zinc, beans, milk, and other dairy products.
 
-Consectetur adipiscing elit duis tristique sollicitudin nibh. Dictumst quisque sagittis purus sit amet volutpat. Quis enim lobortis scelerisque fermentum dui faucibus in ornare. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus. Mauris pharetra et ultrices neque. Leo duis ut diam quam nulla porttitor. Sit amet facilisis magna etiam tempor orci eu lobortis elementum. Blandit massa enim nec dui. Aliquam id diam maecenas ultricies mi eget mauris. Mattis rhoncus urna neque viverra justo nec ultrices. Nibh tellus molestie nunc non blandit. Non diam phasellus vestibulum lorem. Ut diam quam nulla porttitor massa id neque aliquam. Eget mauris pharetra et ultrices neque ornare aenean euismod. Urna id volutpat lacus laoreet non curabitur gravida arcu ac. Tellus in hac habitasse platea dictumst vestibulum.
+\n A Diabetes simply means eating healthiest foods in moderate amounts
+and sticking to regular mealtimes
+.Legumes such as beans and peas
+.Whole Grains
+.Avocados
+.Nuts
+.Canola,Olive and peanut oils
+.Low-fat dairy products , such as milk and cheese
 
-Adipiscing commodo elit at imperdiet. Mollis nunc sed id semper risus. Magna fermentum iaculis eu non. Ut pharetra sit amet aliquam id diam maecenas ultricies mi. Amet consectetur adipiscing elit ut aliquam purus sit amet. Ultrices sagittis orci a scelerisque purus semper. Pharetra magna ac placerat vestibulum lectus mauris ultrices eros in. Massa tempor nec feugiat nisl pretium. Etiam dignissim diam quis enim lobortis scelerisque. Duis ut diam quam nulla.
-
-Ultricies lacus sed turpis tincidunt id aliquet risus feugiat. Vitae aliquet nec ullamcorper sit amet risus. Malesuada nunc vel risus commodo viverra. Id aliquet risus feugiat in ante metus dictum at tempor. Commodo sed egestas egestas fringilla phasellus faucibus scelerisque. Ipsum dolor sit amet consectetur. In nibh mauris cursus mattis molestie a. Duis at consectetur lorem donec massa sapien faucibus et molestie. Amet nisl purus in mollis nunc sed id. Sagittis vitae et leo duis ut. Purus in mollis nunc sed id. Mauris ultrices eros in cursus turpis.''');
+\n Diet recommendation for pregnant women
+•	Dried beans.
+•	Dried fruits, such as apricots.
+•	Egg yolk, Oysters (pregnant women should eat them cooked), Poultry.
+•	Some whole grain cereals, if fortified with iron.
+•	Cherries, Apples, Pears, Watermelon, Apricots.  ''');
 
   @override
   Widget build(BuildContext context) {
